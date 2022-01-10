43fac3d0adea9f8e32b8fe850f56b189021238f9
Allen <allen4atsi@gmail.com>
2022-01-09 20:31:38 -0500

    Demo SVG-based animation

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,17 @@ function view(
   state$: Stream<{totalTrains: number}>
   , addTrainLinesVdom$: Stream<any>
   , trainLineVdom$: Stream<any>
+  , timer1$: Stream<any>
+  , timer2$: Stream<any>
 ) {
-  return xs.combine(state$, addTrainLinesVdom$, trainLineVdom$)
+  return xs.combine(state$, addTrainLinesVdom$, trainLineVdom$, timer1$, timer2$)
     .map((
-      [state, addTrainLinesVdom, trainLineVdoms]: [
+      [state, addTrainLinesVdom, trainLineVdoms, timer1, timer2]: [
         {totalTrains: number}
         , any
         , any
+        , number
+        , number
       ]
     ) =>
       h(Container, [
@@ -31,6 +35,25 @@ function view(
         , addTrainLinesVdom
         , ...trainLineVdoms
         , <Segment>Total trains: {state.totalTrains}</Segment>
+        , <Segment>
+          <svg viewBox="0 0 10000 10000" preserveAspectRatio="xMidYMid slice">
+            <rect
+              x="0" y="0"
+              width="10000" height="10000"
+              style={{fill: "#000000FF"}}
+            />
+            <rect
+              x={timer1} y="1000"
+              width="1000" height="1000"
+              style={{fill: "#00770077"}}
+            />
+            <rect
+              x={timer2} y="1500"
+              width="1000" height="1000"
+              style={{fill: "#77000077"}}
+            />
+          </svg>
+        </Segment>
       ])
     )
   ;
@@ -88,7 +111,7 @@ function main(sources: any) {
 
   const state$ = model(trainLineValues$) ;
 
-  const vdom$ = view(state$, addTrainLinesVdom$, trainLineVdom$) ;
+  const vdom$ = view(state$, addTrainLinesVdom$, trainLineVdom$, sources.timer1, sources.timer2) ;
 
   return {
     react: vdom$
@@ -96,6 +119,29 @@ function main(sources: any) {
 
 }
 
-const App = makeComponent(main) ;
+const App = makeComponent(main, {
+  timer1: () => xs
+    .periodic(50)
+    .fold(
+      ({i, up}, c) => {
+        if (i === 0 && !up) return {i: 0, up: true} ;
+        else if (i === 1000 && up) return {i: 1000, up: false} ;
+        else return {i: up ? i + 10 : i - 10, up}
+      }
+      , {i: 0, up: true}
+    )
+    .map(({i}) => i)
+  , timer2: () => xs
+    .periodic(50)
+    .fold(
+      ({i, up}, c) => {
+        if (i === 0 && !up) return {i: 0, up: true} ;
+        else if (i === 1000 && up) return {i: 1000, up: false} ;
+        else return {i: up ? i + 10 : i - 10, up}
+      }
+      , {i: 500, up: true}
+    )
+    .map(({i}) => i)
+}) ;
 
 export default App;
